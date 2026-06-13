325a63a8f0bf15f1d365e1a7c5a32d49478e7ece
pankaj chauhan <pankajjune27@gmail.com>
2026-06-13 21:01:24 +0530

    solved leetcode problem22 - generate parentheses

---
+++ day4/problem3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void solve(int open, int close, string current, vector<string>& ans) {
+    if (open == 0 && close == 0) {
+        ans.push_back(current);
+        return;
+    }
+
+    if (open > 0) {
+        solve(open - 1, close, current + "(", ans);
+    }
+
+    if (close > open) {
+        solve(open, close - 1, current + ")", ans);
+    }
+}
+
+vector<string> generateParenthesis(int n) {
+    vector<string> ans;
+    solve(n, n, "", ans);
+    return ans;
+}
+
+int main() {
+    int n = 3;
+
+    vector<string> result = generateParenthesis(n);
+
+    for (string s : result) {
+        cout << s << endl;
+    }
+
+    return 0;
+}
